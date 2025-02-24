1a05666275f15cbfd4b673448388489ce56d329c
Yash Verma <114978813+Rajput-xv@users.noreply.github.com>
2025-02-25 01:32:20 +0530

    update hospital

--- client/src/pages/Hospital.js
+++ client/src/pages/Hospital.js
@@ -23,8 +23,8 @@ function Hospital(){
     // Function to fetch all hospitals from the API
     const fetchAllHospitals = async () => {
         try {
-            const response = await axios.get(`api/hospital`);
-            console.log("Fetched all hospitals:", response.data); // Log the fetched data
+            const response = await axios.get(process.env.REACT_APP_API_URL+`api/hospital`);
+            // console.log("Fetched all hospitals:", response.data); // Log the fetched data
             setAllHospitals(response.data); // Update the allHospitals state with the API response
         } catch (err) {
             console.error("Error fetching all hospitals:", err); // Log error for debugging
@@ -38,8 +38,8 @@ function Hospital(){
         setHospitals([]); // Clear previous hospitals data before the new request
     
         try {
-            const response = await axios.get(`api/hospital/${city}`);
-            console.log("Fetched hospitals for city:", city, response.data); // Log the fetched data
+            const response = await axios.get(process.env.REACT_APP_API_URL+`api/hospital/${city}`);
+            // console.log("Fetched hospitals for city:", city, response.data); // Log the fetched data
             setHospitals(response.data); // Update the hospital state with the API response
             setError(""); // Reset error if successful
         } catch (err) {
